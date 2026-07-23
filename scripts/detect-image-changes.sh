5693dc11a20dd798321e1f1072551f5f3e2c1cb1
droguljic <1875821+droguljic@users.noreply.github.com>
2026-07-23 09:20:46 +0200

    fix: run security gate after image matrix (#36)
    Drop non-working dynamic requires mutation and statically define
    the requires stanza in the image continuation workflow for the
    `update-security-pr-gate` job ensuring it runs only after all image
    `build-and-push` matrix jobs.

--- scripts/detect-image-changes.sh
+++ scripts/detect-image-changes.sh
@@ -2,7 +2,6 @@
 
 CONTINUE_CONFIG_PATH=".circleci/push-image.yml"
 CHANGES_PARAM_KEY=".workflows.push-image-to-hub.jobs[0].write-build-push.matrix.parameters.env_file_path"
-GATE_REQUIRES_KEY=".workflows.push-image-to-hub.jobs[1].update-security-pr-gate.requires"
 DIFF_JSON_ARR=$(git diff --name-only --diff-filter=AM HEAD~1 |
   grep -E '^node/(secrets|security)/[0-9]+$' |
   jq -R -c -s 'split("\n")[:-1]')
@@ -23,6 +22,5 @@ fi
 echo "Detected image changes: ${DIFF_JSON_ARR}"
 
 yq "${CHANGES_PARAM_KEY}=${DIFF_JSON_ARR}" -i "${CONTINUE_CONFIG_PATH}"
-yq "${GATE_REQUIRES_KEY}=[\"write-build-push\"]" -i "${CONTINUE_CONFIG_PATH}"
 
 echo "Updated ${CONTINUE_CONFIG_PATH} with image changes"
